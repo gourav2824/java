3613f6046b50e20341d19b318c723385e7ba74be
Gourav <gourav2824@gmail.com>
2018-10-21 17:59:29 +0530

    Coin Change Using DP

---
+++ Lecture 24 - DP 4/src/Coin_Change_DP.java
@@ -0,0 +1,47 @@
+
+public class Coin_Change_DP {
+
+	public static void main(String[] args) {
+		
+		int[] coins = {2, 3, 5, 6};
+		int target = 10;
+		
+		CoinChangePermutations(coins, target);
+		CoinChangeCombinations(coins, target);
+	}
+	
+	private static void CoinChangePermutations(int[] coins, int target) {
+		
+		int[] strg = new int[target + 1];
+		
+		strg[0] = 1;
+		
+		for(int i = 1; i < strg.length; i++) {
+			for(int coin : coins) {
+				if(i >= coin) {
+					strg[i] += strg[i - coin];
+				}
+			}
+		}
+		
+		System.out.println(strg[target]);
+	}
+	
+	private static void CoinChangeCombinations(int[] coins, int target) {
+		
+		int[] strg = new int[target + 1];
+		
+		strg[0] = 1;
+		
+		for(int coin : coins) {
+			for(int i = 1; i < strg.length; i++) {
+				if(i >= coin) {
+					strg[i] += strg[i - coin];
+				}
+			}
+		}
+		
+		System.out.println(strg[target]);
+	}
+
+}
