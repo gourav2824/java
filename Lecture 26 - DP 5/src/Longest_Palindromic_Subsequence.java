4fdfacce09a6c215ec182205449156912011a5d1
Gourav <gourav2824@gmail.com>
2018-10-27 20:01:48 +0530

    Longest Palindromic Subsequence

--- Lecture 26 - DP 5/src/Longest_Palindromic_Subsequence.java
+++ Lecture 26 - DP 5/src/Longest_Palindromic_Subsequence.java
@@ -3,12 +3,40 @@ public class Longest_Palindromic_Subsequence {
 
 	public static void main(String[] args) {
 		
-		
+		String str = "abckczbyc";
+		System.out.println(LPS(str, 0, str.length() - 1, new Integer[str.length()][str.length()]));
 	}
 	
-	private static void LPS() {
-		// TODO Auto-generated method stub
-
+	private static int LPS(String str, int i, int j, Integer[][] qb) {
+		
+		if(i > j) {
+			return 0;
+		}
+		
+		if(i == j) {
+			return 1;
+		}
+		
+		if(qb[i][j] != null) {
+			return qb[i][j];
+		}
+		
+		char cs = str.charAt(i);
+		char ce = str.charAt(j);
+		
+		int ans = 0;
+		if(cs == ce) {
+			ans = 2 + LPS(str, i + 1, j - 1, qb);
+		}
+		
+		else {
+			int f1 = LPS(str, i, j - 1, qb);
+			int f2 = LPS(str, i + 1, j, qb);
+			
+			ans = Math.max(f1, f2);
+		}
+		
+		qb[i][j] = ans;
+		return ans;
 	}
-
 }
