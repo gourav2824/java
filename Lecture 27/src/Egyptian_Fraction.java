cb20d96e2726b67dc2c89a42c26b149d035e90ea
Gourav <gourav2824@gmail.com>
2018-10-28 16:59:48 +0530

    Egyptian Fraction

---
+++ Lecture 27/src/Egyptian_Fraction.java
@@ -0,0 +1,25 @@
+
+public class Egyptian_Fraction {
+
+	public static void main(String[] args) {
+		
+		egyptianFraction(3, 7);
+	}
+	
+	private static void egyptianFraction(int num, int denom) {
+		
+		if(num == 0) {
+			return;
+		}
+		
+		if(num == 1) {
+			System.out.println(num + " / " + denom);
+			return;
+		}
+		
+		int newdenom = (int)Math.ceil(denom * 1.0 / num);
+		int newnum = 1;
+		System.out.println(1 + " / " + newdenom);
+		egyptianFraction(num * newdenom - denom, newdenom * denom);
+	}
+}
