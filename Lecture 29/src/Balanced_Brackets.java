f8e4b8315360329ddfebf532bbba0e7e48d038d5
Gourav <gourav2824@gmail.com>
2018-11-04 19:33:18 +0530

    Balanced Brackets

---
+++ Lecture 29/src/Balanced_Brackets.java
@@ -0,0 +1,41 @@
+
+public class Balanced_Brackets {
+
+	public static void main(String[] args) {
+		
+		String str = "[{(a+b)+(c+d)+e}+f])";
+		
+		System.out.println(balancedBrackets(str));
+	}
+	
+	private static boolean balancedBrackets(String str) {
+		
+		Stack st = new Stack(str.length());
+		
+		String obs = "({[";
+		String cbs = ")}]";
+		
+		for(char ch : str.toCharArray()) {
+			
+			if(obs.indexOf(ch) != -1) {
+				st.push(ch);
+			}
+			
+			else if(cbs.indexOf(ch) != -1) {
+				if(st.size() == 0) {		// more closing brackets
+					return false;
+				}
+				else if(obs.indexOf(st.top()) != cbs.indexOf(ch)) {
+					return false;
+				}
+				st.pop();
+			}
+		}
+		
+		if(st.size() != 0) {		// more opening brackets
+			return false;
+		}
+		
+		return true;
+	}
+}
