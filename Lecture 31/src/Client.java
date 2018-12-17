dd52dbaaa7c0d983b5c7fee0e31301f6f4c79e9d
Gourav <gourav2824@gmail.com>
2018-12-17 17:02:59 +0530

    Lecture 31

---
+++ Lecture 31/src/Client.java
@@ -0,0 +1,15 @@
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		Linked_List list = new Linked_List();
+		
+		list.addLast(10);
+		list.addLast(20);
+		list.addLast(30);
+		list.addLast(40);
+		
+		list.display();
+	}
+}
--- Lecture 31/src/Linked_List.java
+++ Lecture 31/src/Linked_List.java
@@ -1,4 +1,77 @@
 
 public class Linked_List {
-
+	
+	private class Node{
+		
+		int data;
+		Node next;
+	}
+	
+	Node head;
+	Node tail;
+	int size;
+	
+	public int getFirst() {
+		
+	}
+	
+	public int getLast() {
+		
+	}
+	
+	public int getAt(int idx) {
+		
+	}
+	
+	public void addFirst(int data) {
+		
+	}
+	
+	public void addLast(int data) {
+		
+		Node node = new Node();
+		node.data = data;
+		
+		if(size == 0) {
+			head = tail = node;
+		}
+		
+		else {
+			tail.next = node;
+			tail = node;
+		}
+		size++;
+	}
+	
+	public void addAt(int data, int idx) {
+		
+	}
+	
+	public int removeFirst() {
+		
+	}
+	
+	public int removeLast() {
+		
+	}
+	
+	public int removeAt(int idx) {
+		
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	public void display() {
+		
+		for(Node node = head; node != null; node = node.next) {
+			System.out.print(node.data + " -> ");
+		}
+		System.out.println(".");
+	}
 }
