65cf300b941b6df95a7541976232994376680daa
Gourav <gourav2824@gmail.com>
2018-12-27 19:04:29 +0530

    BST Creation

---
+++ Lecture 39 - BST/src/Client_BST.java
@@ -0,0 +1,13 @@
+
+public class Client_BST {
+
+	public static void main(String[] args) {
+		
+		int[] sa = {12, 25, 37, 50, 62, 75, 87};
+		
+		BST bst = new BST(sa);
+		
+		bst.display();
+	}
+
+}
