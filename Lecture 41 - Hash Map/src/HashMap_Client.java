c05c9707c17cbced4b823f00c5d669d9775c535f
Gourav <gourav2824@gmail.com>
2018-12-30 19:50:11 +0530

    Get Common Elements

--- Lecture 41 - Hash Map/src/HashMap_Client.java
+++ Lecture 41 - Hash Map/src/HashMap_Client.java
@@ -4,12 +4,19 @@ public class HashMap_Client {
 
 	public static void main(String[] args) {
 		
-		highestFrequencyCharacter("abbaccbbaabbddbb");
+//		highestFrequencyCharacter("abbaccbbaabbddbb");
+		
+		int[] one = {5, 1, 3, 1, 2, 2, 1};
+		int[] two = {2, 2, 4, 1, 1, 5, 2};
+		
+		getCommonElements1(one, two);
+		getCommonElements2(one, two);
 	}
 	
 	public static void highestFrequencyCharacter(String word) {
 		
 		HashMap<Character, Integer> frequencies = new HashMap<>();
+		
 		Integer maxFreq = Integer.MIN_VALUE;
 		Character ch = word.charAt(0);
 		
@@ -34,5 +41,41 @@ public class HashMap_Client {
 		}
 		
 		System.out.println(ch);
+	}
+	
+	public static void getCommonElements1(int[] one, int[] two) {
+		
+		HashMap<Integer, Integer> freqMap = new HashMap<>();
+		
+		for(int i : one) {
+			freqMap.put(i, freqMap.containsKey(i) ? freqMap.get(i) + 1: 1);
+		}
+		
+		for(int val : two) {
+			if(freqMap.containsKey(val)) {
+				System.out.print(val + " ");
+				freqMap.remove(val);
+			}
+		}
+		
+		System.out.println();
+	}
+	
+	public static void getCommonElements2(int[] one, int[] two) {
+		
+		HashMap<Integer, Integer> freqMap = new HashMap<>();
+		
+		for(int i : one) {
+			freqMap.put(i, freqMap.containsKey(i) ? freqMap.get(i) + 1: 1);
+		}
+		
+		for(int val : two) {
+			if(freqMap.containsKey(val) && freqMap.get(val) > 0) {
+				System.out.print(val + " ");
+				freqMap.put(val, freqMap.get(val) - 1);
+			}
+		}
+		
+		System.out.println();
 	}
 }
