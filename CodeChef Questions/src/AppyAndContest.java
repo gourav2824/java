cf9c003e843ff98e7b4a098f557bbc07d2cb7b53
Gourav <gourav2824@gmail.com>
2019-02-08 01:00:37 +0530

    Appy And Contest

--- CodeChef Questions/src/AppyAndContest.java
+++ CodeChef Questions/src/AppyAndContest.java
@@ -1,9 +1,46 @@
+import java.util.Scanner;
 
 public class AppyAndContest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		int testCases = sc.nextInt();
+		
+		int testCase = 1;
+		while(testCase <= testCases) {
+			
+			long n = sc.nextLong();
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			long k = sc.nextLong();
+			
+			long na = n / a;
+			long nb = n / b;
+			
+			long lcm = a * b;
+			
+			if(a % b == 0) {
+				lcm = a;
+			}
+			
+			if(b % a == 0) {
+				lcm = b;
+			}
+			
+			long nlcm = n / lcm;
+			
+			long prob = (na + nb) - (2 * nlcm);
+			
+			if(prob >= k) {
+				System.out.println("Win");
+			}
+			
+			else {
+				System.out.println("Lose");
+			}
+			
+			testCase ++;
+		}
 	}
-
 }
