c3c4a1e36e5de0a656c81f92c8d9963327c6c8a4
Gourav <gourav2824@gmail.com>
2019-03-03 04:12:51 +0530

    Chef And Beautiful Digit

--- CodeChef Questions/src/ChefAndBeautifulDigit.java
+++ CodeChef Questions/src/ChefAndBeautifulDigit.java
@@ -10,7 +10,7 @@ public class ChefAndBeautifulDigit {
 		
 		while(tno < t) {
 			
-			long n = sc.nextLong();			// 5438932167328
+			long n = sc.nextLong();
 			long d = sc.nextLong();
 			
 			long quo = n;
@@ -24,25 +24,14 @@ public class ChefAndBeautifulDigit {
 			long num = n;
 			long i = len;
 			
-//			int time = 40;
-			
 			while(i > 0) {
-				
-//				long digit = (long)(num / Math.pow(10, i-1));
-				
-//				long high = (long)(num / Math.pow(10, i));
-//				long low = (long)(num % Math.pow(10, i-1));
 				
 				long div1 = (long) (Math.pow(10, i));
 				long div2 = (long) (Math.pow(10, i-1));
 				
-//				System.out.println("div1 = " + div1 + " & div2 = " + div2);
-				
 				long high = num / div1;
 				long low = num % div2;
-				
-//				System.out.println("high = " + high + " & low = " + low);
-				
+								
 				quo = low;
 				long lowlen = 0;
 				
@@ -50,9 +39,7 @@ public class ChefAndBeautifulDigit {
 					quo = quo / 10;
 					lowlen ++;
 				}
-				
-//				long temp = (long)(high * Math.pow(10, lowlen));
-				
+								
 				long mul = (long) Math.pow(10, lowlen);
 				long temp = high * mul;
 				
@@ -60,9 +47,7 @@ public class ChefAndBeautifulDigit {
 				
 				temp = temp * 10;
 				temp = temp + d;
-				
-//				System.out.println("temp = " + temp);
-				
+								
 				if(temp < num) {
 					num = temp;
 					i = len;
@@ -71,17 +56,8 @@ public class ChefAndBeautifulDigit {
 				else {
 					i--;
 				}
-				
-//				System.out.println("num  = " + num);
-//				System.out.println();
-				
-//				time --;
-//				if(time == 0) {
-//					break;
-//				}
 			}
 			
-//			System.out.println("Answer = " + num);
 			System.out.println(num);
 			tno ++;
 		}
